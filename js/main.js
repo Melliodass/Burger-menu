313a6c79c0d687e893f376aa189844981ba61e30
Melliodass <41867039+Melliodass@users.noreply.github.com>
2020-11-08 14:09:57 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-
-});
-$(document).ready(function(){
-
-});
-$(document).ready(function(){
-   
-});
-$(document).ready(function(){
-   $('') ;
-});
-$('');
-;
-
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+let list = document.querySelector('.header__list');
+let body = document.querySelector('body');
+burger.addEventListener('click', function(){
+ 
+  menu.classList.toggle('active');
+  list.classList.toggle('active');
+  burger.classList.toggle('active');
+  body.classList.toggle('lock');
+})
+window.onresize = function(event) {
+  if (screen.availWidth > 768) { 
+    menu.classList.remove('active');
+    list.classList.remove('active');
+   }
+};
